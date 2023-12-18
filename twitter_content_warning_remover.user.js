ec8ba6165ed0130882c3cad7e7df942ab33b4443
Kethsar <kethstuff@gmail.com>
2023-12-18 12:42:50 -0800

    Fix content warning removal for new media tab; use CSS instead of removing the element from the DOM

--- twitter_content_warning_remover.user.js
+++ twitter_content_warning_remover.user.js
@@ -2,8 +2,9 @@
 // @name        Twitter Content Warning Removal
 // @description Remove the retarded content warning shit from media
 // @author      Kethsar
-// @version     1.0
+// @version     1.1
 // @match       https://twitter.com/*
+// @match       https://x.com/*
 // @inject-into auto
 // @updateURL   https://raw.githubusercontent.com/Kethsar/twitter_image_links/master/twitter_content_warning_remover.user.js
 // @downloadURL https://raw.githubusercontent.com/Kethsar/twitter_image_links/master/twitter_content_warning_remover.user.js
@@ -13,7 +14,7 @@
     'use strict';
 
     // Liable to break
-    const contentWarnClass = '.r-drfeu3',
+    const contentWarnClass = '.r-1cmwbt1',
           blurClass = '.r-yfv4eo';
     let interval = 0;
 
@@ -36,35 +37,14 @@
 
         clearInterval(interval);
         createStyle();
-        createRootObserver();
         console.log("Content Warning Remover loaded");
     }
 
     function createStyle()
     {
         const css = document.createElement("style");
-        css.innerHTML = `${blurClass} { filter: none !important; }`;
+        css.innerHTML = `${blurClass} { filter: none !important; } ${contentWarnClass} { display: none !important; }`;
         document.head.appendChild(css);
-    }
-
-    function createRootObserver() {
-        const root = document.getElementById("react-root"),
-              obs = new MutationObserver((mlist, obs) => {
-                  setTimeout(handleMutations, 500, mlist);
-              });
-
-        obs.observe(root, {childList: true, subtree: true});
-    }
-
-    function handleMutations(mlist) {
-        mlist.forEach(mtn => {
-            mtn.addedNodes.forEach(an => {
-                const warning = an.querySelector(contentWarnClass);
-                if (warning) {
-                    warning.remove();
-                }
-            });
-        });
     }
 
     init();
